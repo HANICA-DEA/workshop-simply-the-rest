9a89fe9802ebebd817907ccc1af385abb18b12e7
Meron Brouwer <meron.brouwer@gmail.com>
2020-09-21 20:38:04 +0200

    [refactor] removed an unused import

--- src/main/java/nl/han/oose/dea/rest/resources/HealthCheckResource.java
+++ src/main/java/nl/han/oose/dea/rest/resources/HealthCheckResource.java
@@ -2,8 +2,6 @@ package nl.han.oose.dea.rest.resources;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 
 @Path("/health")
 public class HealthCheckResource {
